8ef768f0cfe661681b5f108502bfb5da073316cd
thinhnw <thinhnv0412@gmail.com>
2020-11-13 11:27:22 +0700

    Deploying DB on Atlas

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
+/* Include mongoose to the project */
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/journalDB', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log('Connected to db successfully.');
+});
+
+const postSchema = new mongoose.Schema({
+	title: String,
+	body: String,
+	url: String
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+/**/
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -21,10 +41,13 @@ app.use(express.static("public"));
 
 app.get('/', function(req, res) {
 
-    res.render('home', {
-        homeStartingContent: homeStartingContent,
-        posts: posts
-    });
+	Post.find({}, function(err, posts) {
+
+		res.render('home', {
+			homeStartingContent: homeStartingContent,
+			posts: posts 
+		});
+	});
 });
 
 app.get('/about', function(req, res) {
@@ -44,18 +67,40 @@ app.get('/compose', function(req, res) {
 
 app.get('/posts/:title', function(req, res) {
 
-    posts.forEach((post) => {
-        if (compare(post.title, req.params.title))  {
-			res.render('post', {post: post});
-        }
-    });
+	res.redirect(`/posts/${_.kebabCase(req.params.title)}`);
+
+	Post.findOne({title: req.params.title}, function(err, post) {
+
+		if (err) {
+			return console.error(err);
+		}
+		if (post === null) req.redirect('/');
+		res.render('post', {post: post});
+	})
 });
 
 app.post('/compose', function(req, res) {
 
-	req.body.post.url = _.kebabCase(req.body.post.title);
-    posts.push(req.body.post);
-    res.redirect('/');
+	const post = new Post({
+		title: req.body.post.title,
+		body: req.body.post.body,
+		url: "/posts/" + _.kebabCase(req.body.post.title)
+	});
+
+	const saveFunction = () => {
+
+		post.save(function(err, post) { 
+			if (err) {
+				return console.error(err);
+			}
+			console.log("Add new post ", post);
+		});
+	}
+	const redirect = async () => {
+		await saveFunction();
+		res.redirect('/');
+	};
+	redirect();
 });
 
 
